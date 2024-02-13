52172298b142563834584dedbd50d9a42a799132
alves-hub <jefferson.alv35@gmail.com>
2024-02-13 01:31:54 -0300

    Subindo crianca metodo contrutor para transitar dados entre telas

--- NavigationAvancadaCurso/NavigationAvancadaCurso/Tela02VC.swift
+++ NavigationAvancadaCurso/NavigationAvancadaCurso/Tela02VC.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class Tela02VC: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var secondNameLabel: UILabel!
+    //MARK: CRINDO METODO CONSTRUTOR PARA TRANSITAR DADOS ENTRE TELAS
+    
+    let name:String?
+    let secondName:String?
+    
+    init?(coder: NSCoder, name: String, secondName: String) {
+        self.name = name
+        self.secondName = secondName
+        super.init(coder: coder)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = name
+        secondNameLabel.text = secondName
     }
 
 
--- NavigationAvancadaCurso/NavigationAvancadaCurso/ViewController.swift
+++ NavigationAvancadaCurso/NavigationAvancadaCurso/ViewController.swift
@@ -9,25 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var secondNameTextField: UITextField!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func tappedGoScreenButton(_ sender: UIButton) {
         
+        //MARK: METODO SIMPLES DE NAVEGACAO ENTRE TELAS
         //  >> Criamos uma instancia constante VC
-        let VC: Tela02VC? = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+            //let VC: Tela02VC? = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
         
         //  >> modalPresentationStyle para determinar tela inteira para modal
-        //VC?.modalPresentationStyle = .fullScreen
+            //VC?.modalPresentationStyle = .fullScreen
         
         //  >> ABORDAGEM Para apresentar um MODAL de forma programatica
         //  >> Passando a constante instancia VC
         //  >> "??" - Caso contrario passar UIViewController vazia
         //  >> animated - passando valor para tipo de animacao
-        //self.present(VC ?? UIViewController(), animated: true)
+            //self.present(VC ?? UIViewController(), animated: true)
         
+        
+       
+        
+        //VC?.name = self.nameTextField.text
+        
+        //  >>ABORDAGEM Para apresentar uma NavigationControler de forma programatica
+        //  >> MODO DEBUG INSERINDO EX."PO VC" O MEMSO EXIBI NO TERMINAL O VALOR DA CONSTANTE
+            //self.navigationController?.pushViewController(VC ?? UIViewController(), animated: true)
+        
+        
+        //MARK: TRANSITANDO DADOS ENTRE TELAS
+        
+        //  >> Criamos uma instancia constante VC
+        let VC: Tela02VC? = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(identifier: "Tela02VC") { coder in
+            return Tela02VC(coder: coder, name: self.nameTextField.text ?? "", secondName: self.secondNameTextField.text ?? "")
+            
+        }
+    
+       
+ 
         
         //  >>ABORDAGEM Para apresentar uma NavigationControler de forma programatica
         //  >> MODO DEBUG INSERINDO EX."PO VC" O MEMSO EXIBI NO TERMINAL O VALOR DA CONSTANTE
